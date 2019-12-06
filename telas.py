a1ef75aa7164c0c10cbefa6f80fee01d49ed52c6
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-12-06 14:31:15 -0300

    Add files via upload

--- telas.py
+++ telas.py
@@ -1,9 +1,16 @@
-def telaInicial():  
+from time import sleep
+
+def telaInicial(sens_inicio):
   while (sens_inicio.status() == 0):
-    #Plotar gráficos da tela inicial
-    
+    print("Telainicial")
+  #Plotar gráficos da tela inicial
+
 def telaJogo():
+  print("Tela jogo")
+  sleep(1)
   
 def telaTransicao(vidas):
+  print("Tela transição")
   
 def telaFinal():
+  print("Tela final")
